00f5d5ee69990368a749b89023cb5a90ba57814e
Angelos Kovatsis <aggkov10@hotmail.com>
2022-09-23 19:07:51 +0300

    service and exception package added

---
+++ RenovationAgency/src/main/java/com/europeandynamics/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.europeandynamics.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+
+}
---
+++ RenovationAgency/src/main/java/com/europeandynamics/service/PropertyOwnerService.java
@@ -0,0 +1,5 @@
+package com.europeandynamics.service;
+
+public class PropertyOwnerService {
+
+}

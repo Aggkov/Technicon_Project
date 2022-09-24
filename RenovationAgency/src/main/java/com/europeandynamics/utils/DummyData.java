c3d0deeb30e4b6125de64a61a6896f2c6a82deeb
K.Chatzifotiadis <k.chatzifotiadis@gmail.com>
2022-09-24 14:25:04 +0300

    Created PropertyOwners Objects

---
+++ RenovationAgency/src/main/java/com/europeandynamics/utils/DummyData.java
@@ -0,0 +1,32 @@
+package com.europeandynamics.utils;
+
+import java.time.LocalDate;
+
+import com.europeandynamics.model.Property;
+import com.europeandynamics.model.PropertyOwner;
+import com.europeandynamics.model.enums.Type;
+
+public class DummyData {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	
+
+//		PropertyOwner propertyOwner = new PropertyOwner("111111111", "Kostas", "Chatzifotiadis", "Thessaloniki", "6999999991", "email1@email.com",
+//				"username3", "pass3");
+//		PropertyOwner propertyOwner1 = new PropertyOwner("111111112", "Aggelos", "Kovatsis", "Athens", "6999999992", "email2@email.com",
+//				"username3", "pass3");
+//		PropertyOwner propertyOwner2 = new PropertyOwner("111111113", "Alex", "Johnson", "Xanthi", "6999999993", "email3@email.com",
+//				"username3", "pass3");
+//		PropertyOwner propertyOwner3 = new PropertyOwner("111111114", "Lefteris", "Papageorgiou", "Kavala", "6999999994", "email4@email.com",
+//				"username3", "pass3");
+//		PropertyOwner propertyOwner4 = new PropertyOwner("111111115", "Nikos", "Matzablokos", "Larisa", "6999999995", "email5@email.com",
+//				"username3", "pass3");
+
+	}
+
+}

cdfd13df121d541bcbaf8cc597436a729898822b
Angelos Kovatsis <aggkov10@hotmail.com>
2022-09-28 12:22:26 +0300

    Refactored EntityManagerUtils

--- RenovationAgency/src/main/java/com/europeandynamics/utils/EntityManagerFactoryUtils.java
+++ RenovationAgency/src/main/java/com/europeandynamics/utils/EntityManagerFactoryUtils.java
@@ -1,12 +1,30 @@
 package com.europeandynamics.utils;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class EntityManagerFactoryUtils {
 
+	private static final String PERSISTENCE_UNIT_NAME = "NewPersistenceUnit";
+	private static EntityManagerFactory emf;
+
 	public static EntityManagerFactory getEntityManagerFactory() {
-		return Persistence.createEntityManagerFactory("NewPersistenceUnit");
+		if (emf == null) {
+			emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		}
+		return emf;
+	}
+
+	public static EntityManager getEntityManager() {
+		return getEntityManagerFactory().createEntityManager();
+	}
+
+	public static void shutdown() {
+		if (emf != null) {
+			emf.close();
+			emf = null;
+		}
 	}
 
 }

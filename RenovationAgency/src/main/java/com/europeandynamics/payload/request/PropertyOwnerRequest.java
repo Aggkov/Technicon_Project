12a437c758fada14493b410904172e774ad4ecfb
LefterisCoder <terry_pap@yahoo.gr>
2022-10-03 19:02:00 +0300

    Deleted setters getters

--- RenovationAgency/src/main/java/com/europeandynamics/payload/request/PropertyOwnerRequest.java
+++ RenovationAgency/src/main/java/com/europeandynamics/payload/request/PropertyOwnerRequest.java
@@ -17,27 +17,5 @@ public class PropertyOwnerRequest {
         this.password = password;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+   
 }
